908e94a082dbcc29b4eaace4e6e4265cd68d8032
GXH <18852057361@139.com>
2022-09-26 20:13:50 +0800

    稍微完善版本

---
+++ assets/js/login.js
@@ -0,0 +1,16 @@
+$(function () {
+  $('#go2Rge').on('click', function () {
+    $('.login-wrap').hide()
+    $('.reg-wrap').show()
+  })
+
+  $('#go2Login').on('click', function () {
+    $('.login-wrap').show()
+    $('.reg-wrap').hide()
+  })
+
+
+
+
+  
+})
